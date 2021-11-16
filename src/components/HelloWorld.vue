22e8b6a0d42896e9cdbb4d598d4df75e451a380f
Andy Chen <chenyangpoppin@hotmail.com>
2021-11-16 09:13:21 +0800

    feat: add class component prop and method

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,7 +1,13 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+    <h2>{{ name }}</h2>
     <h3>{{ text }}</h3>
+    <div>
+      <button v-on:click="decrement">-</button>
+      {{ count }}
+      <button v-on:click="increment">+</button>
+    </div>
   </div>
 </template>
 
@@ -12,6 +18,21 @@ import { Component, Prop, Vue } from 'vue-property-decorator';
 export default class HelloWorld extends Vue {
   @Prop() private msg!: string;
   @Prop() private text!: string;
+
+  // Class properties will be component data
+  name = "Andy";
+
+  // Class properties will be component data
+  count = 0;
+
+  // Methods will be component methods
+  increment(): number {
+    return this.count++;
+  }
+
+  decrement(): number {
+    return this.count--;
+  }
 }
 </script>
 
